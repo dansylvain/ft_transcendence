b8b57f66b30a1d576691c22010363142cd6c0d85
Dan Sylvain <dansylvain84@gmail.com>
2025-04-18 19:36:52 +0200

    js files reorganized

--- ultimate_project/static_files/static_files_app/static/static_files/history.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("beforeunload", () => {
-    // console.log("URL sauvegardée avec history.state et sessionStorage");
-	stopMatch();
-    let currentURL = window.location.href;
-    history.replaceState({ lastVisitedPage: currentURL }, "");
-    sessionStorage.setItem("lastVisitedPage", currentURL);
-  });
