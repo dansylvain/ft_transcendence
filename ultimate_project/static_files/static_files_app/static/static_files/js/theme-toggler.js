7a0544791397c8d2a27e2f55c6dfe589aeda96de
Dan Sylvain <dansylvain84@gmail.com>
2025-04-18 19:31:11 +0200

    css reorganized

---
+++ ultimate_project/static_files/static_files_app/static/static_files/js/theme-toggler.js
@@ -0,0 +1,26 @@
+if (typeof themeToggle === 'undefined') {
+    let themeToggle = document.querySelector('.switch .input');
+
+    if (themeToggle) {
+        const htmlEl = document.documentElement;
+        themeToggle.addEventListener('change', () => {
+            if (themeToggle.checked) {
+                htmlEl.setAttribute('data-theme', 'dark');
+            } else {
+                htmlEl.setAttribute('data-theme', 'light');
+            }
+        });
+
+        const savedTheme = localStorage.getItem('theme');
+        if (savedTheme) {
+            htmlEl.setAttribute('data-theme', savedTheme);
+            themeToggle.checked = savedTheme === 'dark';
+        }
+
+        themeToggle.addEventListener('change', () => {
+            const newTheme = themeToggle.checked ? 'dark' : 'light';
+            htmlEl.setAttribute('data-theme', newTheme);
+            localStorage.setItem('theme', newTheme);
+        });
+    }
+}
